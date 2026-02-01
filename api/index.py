ed9b2d5fa7c46fce55e2b6bca4158b77b71b6a4f
RBP <rob_b_paul@hotmail.com>
2026-02-01 18:47:22 +0700

    "pure-function-v140"

--- api/index.py
+++ api/index.py
@@ -1,11 +1,8 @@
-from http.server import BaseHTTPRequestHandler
-
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-Type', 'application/json')
-        self.end_headers()
-        import os
-        import sys
-        self.wfile.write(bytes('{"status":"extreme_baseline", "python":"' + sys.version + '"}', "utf-8"))
-        return
+def handler(request):
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'application/json'
+        },
+        'body': '{"status":"ok_pure_function"}'
+    }
