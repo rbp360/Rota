832d815d900cb27311557c72b42d3b02d4ae6df0
RBP <rob_b_paul@hotmail.com>
2026-02-01 18:45:34 +0700

    "pure-wsgi-test-v138"

---
+++ api/test.py
@@ -0,0 +1,13 @@
+from http.server import BaseHTTPRequestHandler
+import json
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/json')
+        self.end_headers()
+        self.wfile.write(json.dumps({
+            'status': 'pure_wsgi_test',
+            'version': '1.3.8'
+        }).encode())
+        return
